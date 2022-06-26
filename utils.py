dbbbedc0b60178e1aa9f48e225edb5182df306f7
alfianpr <alfianpratama@hotmail.com>
2022-06-27 02:11:27 +0700

    init

--- utils.py
+++ utils.py
@@ -4,5 +4,5 @@ from types import SimpleNamespace
 def df_to_ps (df, table_name, credential={}):
     cre = SimpleNamespace(**credential)
     conn_string = 'postgresql://{}:{}@{}:{}/{}'.format(cre.username, cre.password, cre.host, cre.port, cre.db_name)
-    conn = create_engine(conn_string).connect()
+    conn = create_engine(conn_string)
     return df.to_sql(table_name, conn, if_exists= 'append')
